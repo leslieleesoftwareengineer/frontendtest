62517955aa06d55b1a915b4c7c984214941fc528
Leslie <leslie.lee.softwareengineer@gmail.com>
2022-04-17 15:23:04 +1000

    fix: Unit test

---
+++ movie-monday/src/components/Search/Search.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Search from "./Search";
+
+const onSearchFn = jest.fn();
+test("onSearch function should be run", () => {
+  render(<Search onSearch={onSearchFn} />);
+  const button = screen.getByRole('button');
+  button.click();
+  expect(onSearchFn).toHaveBeenCalled();
+});
